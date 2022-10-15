9ceab6bcfa9383bda4f8ef27b06f63a0306ca026
quangpn7 <nhutquang.mca@gmail.com>
2022-10-15 11:08:26 +0700

    Upload file bai tap

---
+++ js/main.js
@@ -0,0 +1,144 @@
+let dataGlasses = [
+  {
+    id: "G1",
+    src: "./img/g1.jpg",
+    virtualImg: "./img/v1.png",
+    brand: "Armani Exchange",
+    name: "Bamboo wood",
+    color: "Brown",
+    price: 150,
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis ea voluptates officiis? ",
+  },
+  {
+    id: "G2",
+    src: "./img/g2.jpg",
+    virtualImg: "./img/v2.png",
+    brand: "Arnette",
+    name: "American flag",
+    color: "American flag",
+    price: 150,
+    description:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. In assumenda earum eaque doloremque, tempore distinctio.",
+  },
+  {
+    id: "G3",
+    src: "./img/g3.jpg",
+    virtualImg: "./img/v3.png",
+    brand: "Burberry",
+    name: "Belt of Hippolyte",
+    color: "Blue",
+    price: 100,
+    description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit.",
+  },
+  {
+    id: "G4",
+    src: "./img/g4.jpg",
+    virtualImg: "./img/v4.png",
+    brand: "Coarch",
+    name: "Cretan Bull",
+    color: "Red",
+    price: 100,
+    description: "In assumenda earum eaque doloremque, tempore distinctio.",
+  },
+  {
+    id: "G5",
+    src: "./img/g5.jpg",
+    virtualImg: "./img/v5.png",
+    brand: "D&G",
+    name: "JOYRIDE",
+    color: "Gold",
+    price: 180,
+    description:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Error odio minima sit labore optio officia?",
+  },
+  {
+    id: "G6",
+    src: "./img/g6.jpg",
+    virtualImg: "./img/v6.png",
+    brand: "Polo",
+    name: "NATTY ICE",
+    color: "Blue, White",
+    price: 120,
+    description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit.",
+  },
+  {
+    id: "G7",
+    src: "./img/g7.jpg",
+    virtualImg: "./img/v7.png",
+    brand: "Ralph",
+    name: "TORTOISE",
+    color: "Black, Yellow",
+    price: 120,
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim sint nobis incidunt non voluptate quibusdam.",
+  },
+  {
+    id: "G8",
+    src: "./img/g8.jpg",
+    virtualImg: "./img/v8.png",
+    brand: "Polo",
+    name: "NATTY ICE",
+    color: "Red, Black",
+    price: 120,
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, unde enim.",
+  },
+  {
+    id: "G9",
+    src: "./img/g9.jpg",
+    virtualImg: "./img/v9.png",
+    brand: "Coarch",
+    name: "MIDNIGHT VIXEN REMIX",
+    color: "Blue, Black",
+    price: 120,
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit consequatur soluta ad aut laborum amet.",
+  },
+];
+
+let renderProducts = (productArr) => {
+  let contentHTML = "";
+  for (const glass of productArr) {
+    contentHTML += `
+    <div class="col-4">
+              <button class="btn" onclick = "renderGlass('${glass.id}')">
+                <img src="${glass.src}" alt="" style="width: 100%" />
+              </button>
+            </div>
+    `;
+  }
+  document.querySelector("#vglassesList").innerHTML = contentHTML;
+};
+window.renderGlass = (glassID) => {
+  let index = dataGlasses.findIndex((ob) => ob.id === glassID);
+  let glass = dataGlasses[index];
+  document.querySelector("#glassesInfo").innerHTML = `
+            <div class="glasses-name">
+                          <span>${glass.name}</span>
+                          <span> - ${glass.brand}</span>
+                          <span> (${glass.color})</span>
+                        </div>
+                        <div class="glass-status">
+                          <span class="btn btn-danger">$${glass.price}</span>
+                          <span>Stocking</span><br />
+                        </div>
+                        <p>
+                          ${glass.description}
+                        </p>
+            `;
+  document.querySelector("#avatar").innerHTML = `
+<img src="${glass.virtualImg}" alt="vitural-img">
+`;
+  document.querySelector(".vglasses__info").style.display = "block";
+};
+
+renderProducts(dataGlasses);
+
+window.removeGlasses = (value) => {
+  if (value == false) {
+    document.querySelector("#avatar img").style.opacity = 0;
+  } else {
+    document.querySelector("#avatar img").style.opacity = 0.9;
+  }
+};
